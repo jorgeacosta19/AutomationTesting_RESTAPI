566f427b4bed5962a7c64d93711134f7fbdefdea
jorgeacosta19 <jorgeacosta992@gmail.com>
2021-02-22 23:34:58 +0100

    Add project files.

---
+++ PetStore/GetUserByIDTest.cs
@@ -0,0 +1,37 @@
+﻿
+using RestSharp;
+using System.Collections.Generic;
+using NUnit.Framework;
+using RestSharp.Serialization.Json;
+
+
+namespace PetStore
+{
+    [TestFixture]
+    public class GetUserByIDTest
+    {
+        [Test]
+        public void TestGETUserbyID()
+        {
+            var client = new RestClient("https://petstore.swagger.io/v2/");
+            var request = new RestRequest("user/{username}", Method.GET);
+            request.AddUrlSegment("username", "jorge19");
+
+            var response = client.Execute(request);
+
+            //To get rid of the object
+            var deserialize = new JsonDeserializer();
+            var output = deserialize.Deserialize<Dictionary<string, string>>(response);
+            var result = output["lastName"];
+
+            Assert.That(result, Is.EqualTo("Acosta"), "Surname is not correct");
+
+            //To make the test fail 
+            //Assert.That(result, Is.EqualTo("Borg"), "Surname is not correct");
+
+        }
+
+
+    }
+
+}
---
+++ README.txt
@@ -0,0 +1,31 @@
+PetStore
+Open the link https://petstore.swagger.io/#/
+
+1. Create a User as follows: 
+
+{
+  "id": 987654321,
+  "username": "jorge19",
+  "firstName": "Jorge",
+  "lastName": "Acosta",
+  "email": "jorgeacosta992@gmail.com",
+  "password": "123",
+  "phone": "77777777",
+  "userStatus": 1
+}
+
+
+2.Whitin console open this path PetStore\PetStore\ 
+
+3.Type the following command: dotnet test
+
+A GET test will be displayed: Positive Testing with valid parameters.
+
+For this and more Test cases information please check the document:
+
+PetStore_TestPlan.pdf
+
+		
+		
+		
+
